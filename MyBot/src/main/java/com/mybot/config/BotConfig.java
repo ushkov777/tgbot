587c443c1b63c0bf5d3c583be42f4bf51c859771
roma <147475322+ushkov777@users.noreply.github.com>
2025-11-12 09:50:40 +0500

    Update BotConfig.java

--- MyBot/src/main/java/com/mybot/config/BotConfig.java
+++ MyBot/src/main/java/com/mybot/config/BotConfig.java
@@ -1,6 +1,7 @@
 package com.mybot.config;
 
 public class BotConfig {
-    public static final String BOT_TOKEN = "8349274682:AAFE0gZYVS1x1_ydz1X2wI7NRWiIqKCyamI";
-    public static final String BOT_USERNAME = "pogodaNaNedele_bot";
+    public static final String BOT_TOKEN = "Your_Token";
+    public static final String BOT_USERNAME = "Your_Name";
+
 }
